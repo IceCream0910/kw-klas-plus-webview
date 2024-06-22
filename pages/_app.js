5be34cfb1592793a82ad5320f5fe623f211d6713
icecream0910 <taein2370@naver.com>
2024-06-22 17:01:08 +0900

    INIT: first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import "@/styles/globals.css";
+import { useEffect } from 'react';
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    window.receiveToken = function (token) {
+      if (!token) return;
+      localStorage && localStorage.setItem('klas-plus-token', token);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
