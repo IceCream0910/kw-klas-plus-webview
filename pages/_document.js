d429bffce76f73a8284fdc09da3dab164898a249
icecream0910 <taein2370@naver.com>
2024-09-10 11:39:15 +0900

    feat: google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,28 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="ko">
       <Head />
       <body>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js
+				?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <Script
+          id="google-analytics"
+          dangerouslySetInnerHTML={{
+            __html: `
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+		`,
+          }}
+        />
         <Main />
         <NextScript />
       </body>
