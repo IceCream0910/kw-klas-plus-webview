824d14ae5799c92b5be0595e90271f3ff1cd2a23
icecream0910 <taein2370@naver.com>
2024-09-13 01:54:56 +0900

    style: svg fill color on light mode

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,8 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
-      <script defer src="https://cloud.umami.is/script.js" data-website-id="ed89a4a4-7e32-47ad-bc76-b7b9a77795b0"></script>
+      {/*      <script defer src="https://cloud.umami.is/script.js" data-website-id="ed89a4a4-7e32-47ad-bc76-b7b9a77795b0"></script>
+ */}
     </Html>
   );
 }
