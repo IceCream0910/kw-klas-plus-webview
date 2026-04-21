33a043520e1560a1d0ae447b6ec3c6729a10d2af
Yun Taein <hey@yuntae.in>
2026-04-21 20:51:37 +0900

    fix: font import 누락

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
       <Head>
         <link rel="preload" href="https://cdn.jsdelivr.net/gh/toss/tossface/dist/tossface.css" as="style" />
         <link rel="preload" href="https://cdn.jsdelivr.net/gh/wanteddev/wanted-sans@v1.0.3/packages/wanted-sans/fonts/webfonts/variable/split/WantedSansVariable.min.css" as="style" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/wanteddev/wanted-sans@v1.0.3/packages/wanted-sans/fonts/webfonts/variable/split/WantedSansVariable.min.css" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/toss/tossface/dist/tossface.css" />
       </Head>
       <body>
